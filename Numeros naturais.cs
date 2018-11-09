8a1775c9357ce09ab4589acbde2684466a337b33
Felipe <dudmont1@gmail.com>
2018-11-09 05:06:49 -0200

    N[umeros naturais de 1 a 100

---
+++ Numeros naturais.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Numeros naturais entre 1 a 100");
+
+            int soma = 0;
+
+            for (int x = 1; x <= 100; x++) 
+            {
+                soma = soma + x;
+                Console.WriteLine(x);
+            }
+
+            Console.WriteLine("A soma de todos os naturais até 100 é =" +  soma);
+           
+
+            Console.ReadKey(); 
+        }
+    }
+}
